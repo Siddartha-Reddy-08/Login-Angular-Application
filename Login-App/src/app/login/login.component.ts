870a2731688a4be1a6b00973baa9c1819f9711ff
Siddartha Reddy <Siddartha8523@gmail.com>
2024-02-11 16:14:02 +0530

    login

--- Login-App/src/app/login/login.component.ts
+++ Login-App/src/app/login/login.component.ts
@@ -5,46 +5,49 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
   username: String = '';
   password: String = '';
   hide = true;
   userData: any;
-  constructor(private servicesService: ServicesService, private router: Router) { }
+  constructor(
+    private servicesService: ServicesService,
+    private router: Router
+  ) {}
 
   setUserData() {
-
     this.servicesService.getUsersData().subscribe(
-      data => {
+      (data) => {
         this.userData = data;
         console.log(data);
         this.checkDetails();
       },
-      error => {
+      (error) => {
         console.log('ERROR', error);
       }
-    )
+    );
   }
 
   checkDetails(): void {
-    this.userData.forEach((element: { username: String; password: String }): void => {
-      if (element.username === this.username && element.password === this.password) {
-        console.log(element.username);
-        this.router.navigateByUrl('/insert');
+    this.userData.forEach(
+      (element: { username: String; password: String }): void => {
+        if (
+          element.username === this.username &&
+          element.password === this.password
+        ) {
+          console.log(element.username);
+          this.router.navigateByUrl('/insert');
+        } else {
+          console.log('Incorrect Details');
+          this.router.navigateByUrl('/');
+        }
       }
-      else {
-        console.log('Incorrect Details');
-        this.router.navigateByUrl('/');
-      }
-    });
+    );
   }
-
 
   getRegister() {
     this.router.navigateByUrl('/register');
   }
-
 }
